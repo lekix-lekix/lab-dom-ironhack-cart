200e074c25dae36517bb70ce1ee485ed3f34747d
lekix-lekix <pouliquen.k@gmail.com>
2023-01-29 22:13:56 +0100

    done but cant make the bonus work

--- src/index.js
+++ src/index.js
@@ -1,42 +1,91 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = Number(product.querySelector(".price span").innerHTML);
+  const quantity = Number(product.querySelector(".quantity input").value);
+  const subTotal = price * quantity;
+  product.querySelector(".subtotal span").textContent = subTotal;
+  return subTotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.getElementsByClassName("product");
+  let total = 0;
+  for (const product of allProducts) {
+    total += updateSubtotal(product);
+  }
+  document.getElementById("total-value").lastChild.textContent = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
+  target.closest(".product").remove();
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const name = document.querySelector('.create-product :nth-child(1) input').innerHTML;
+  const price = Number(document.querySelector('.create-product :nth-child(2) input').innerHTML);
+  const tr = document.querySelector('.product').cloneNode(true);
+  const test = document.createElement("tr");
+  tr.querySelector('.name span').textContent = name;
+  tr.querySelector('.price span').textContent = price;
+  const tbody = document.getElementsByTagName('tbody');
+  console.log(tbody);
+  tbody.append(test);
+  // for (let i = 0; i < 5; i++) {
+  //   switch (i) {
+  //     case 0:
+  //       const td0 = document.createElement('td').classList.add('name');
+  //       const span0 = document.createElement('span').textContent = name;
+  //       td0.appendChild(span0);
+  //       tr.appendChild(td0);
+  //       break;
+  //     case 1: 
+  //       const td1 = document.createElement('td').classList.add('price');
+  //       td1.textContent = '$';
+  //       const span1 = document.createElement('span').textContent = price;
+  //       td1.appendChild(span1);
+  //       tr.appendChild(td1);
+  //       break;
+  //     case 2:
+  //       const td2 = document.createElement('td').classList.add('quantity');
+  //       const input = document.querySelector('quantity').clodeNode(deep);
+  //       td2.appendChild(input);
+  //       tr.appendChild(td2);
+  //       break;
+  //     case 3:
+  //       const td3 = document.createElement('td').classList.add('subtotal');
+  //       td3.textContent = '$';
+  //       const span3 = document.createElement('span');
+  //       span3.textContent = '0';
+  //       td3.appendChild(span3);
+  //       tr.appendChild()
+  //       break;
+  //     case 4:
+  //       const td4 = document.createElement.classList.add('action');
+  //       const button = document.createElement.classList.add('btn btn-remove');
+  //       button.textContent = 'Remove';
+  //       break;
+  //     }
+  
 }
+  
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
+  
+  const removeButton = document.querySelectorAll(".btn.btn-remove"); 
+  removeButton.forEach((button) => {
+    button.addEventListener('click', event => {
+      removeProduct(event);
+    }
+  )});
+  
+  const createButton = document.getElementById("create");
+  createButton.addEventListener('click', createProduct());
+  });
